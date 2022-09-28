c05cbb9dafa06dffed837fbe07916ce5ce9cc79d
derodero24 <k.itsh20524abc@gmail.com>
2022-09-28 23:57:26 +0900

    add datepicker

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -3,7 +3,7 @@ import ThemeButton from '../elements/ThemeButton';
 export default function Header() {
   return (
     <header className="px-6 py-4">
-      <nav className="container mx-auto">
+      <nav className="mx-auto max-w-2xl">
         <ThemeButton className="float-right cursor-pointer text-xl hover:opacity-75" />
       </nav>
     </header>
