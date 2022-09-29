d824a1bda53cac15ec2fee7e0c171023a08dbd19
derodero24 <k.itsh20524abc@gmail.com>
2022-09-29 11:39:30 +0900

    update

--- src/lib/days.ts
+++ src/lib/days.ts
@@ -2,40 +2,24 @@ import * as holiday_jp from '@holiday-jp/holiday_jp';
 import { addBusinessDays } from 'date-fns';
 
 /**
- * 作業完了予定日を返す
+ * 祝日を考慮して◯営業日後の日付を返す
+ * @param date
  * @param businessDaysLater
  * @returns
  */
 export const getCompletionDate = (
-  eventDay: Date,
-  businessDaysLater: number,
-) => {
-  // 基準日から◯営業日以降なので、本日+1日からスタート
-  return shiftDate(addBusinessDays(eventDay, 1), businessDaysLater, 0);
-};
-
-/**
- * 祝日を考慮して◯営業日後の日付を返す
- * @param date
- * @param businessDaysLater
- * @param count
- * @returns
- */
-const shiftDate = (
   date: Date,
   businessDaysLater: number,
-  count: number,
 ): Date => {
   if (holiday_jp.isHoliday(date)) {
     date = addBusinessDays(date, 1);
-    return shiftDate(date, businessDaysLater, count);
+    return getCompletionDate(date, businessDaysLater);
   }
 
-  count++;
-  if (count === businessDaysLater) {
+  if (businessDaysLater === 0) {
     return date;
   }
 
   date = addBusinessDays(date, 1);
-  return shiftDate(date, businessDaysLater, count);
+  return getCompletionDate(date, businessDaysLater - 1);
 };
