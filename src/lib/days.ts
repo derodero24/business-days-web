66e998e7a23bddf1887a0daa528ea7d476c09e87
derodero24 <k.itsh20524abc@gmail.com>
2022-09-29 13:32:24 +0900

    add holiday options

--- src/lib/days.ts
+++ src/lib/days.ts
@@ -1,5 +1,16 @@
 import * as holiday_jp from '@holiday-jp/holiday_jp';
-import { addBusinessDays } from 'date-fns';
+import * as fns from 'date-fns';
+
+export interface HolidaysOption {
+  mon: boolean;
+  tue: boolean;
+  wed: boolean;
+  thu: boolean;
+  fri: boolean;
+  sat: boolean;
+  sun: boolean;
+  jp: boolean;
+}
 
 /**
  * 祝日を考慮して◯営業日後の日付を返す
@@ -7,19 +18,45 @@ import { addBusinessDays } from 'date-fns';
  * @param businessDaysLater
  * @returns
  */
-export const getCompletionDate = (
+export function getCompletionDate(
   date: Date,
   businessDaysLater: number,
-): Date => {
-  if (holiday_jp.isHoliday(date)) {
-    date = addBusinessDays(date, 1);
-    return getCompletionDate(date, businessDaysLater);
+  holidays: HolidaysOption,
+): Date {
+  // 全て休日はNG
+  if (isAllDayOff(holidays)) {
+    throw new Error('Do not set every day as a holiday.');
   }
 
+  // 休日はスキップ
+  if (
+    (holidays.mon && fns.isMonday(date)) ||
+    (holidays.tue && fns.isTuesday(date)) ||
+    (holidays.wed && fns.isWednesday(date)) ||
+    (holidays.thu && fns.isThursday(date)) ||
+    (holidays.fri && fns.isFriday(date)) ||
+    (holidays.sat && fns.isSaturday(date)) ||
+    (holidays.sun && fns.isSunday(date)) ||
+    (holidays.jp && holiday_jp.isHoliday(date))
+  ) {
+    date = fns.addDays(date, 1);
+    return getCompletionDate(date, businessDaysLater, holidays);
+  }
+
+  // 終了条件
   if (businessDaysLater === 0) {
     return date;
   }
 
-  date = addBusinessDays(date, 1);
-  return getCompletionDate(date, businessDaysLater - 1);
-};
+  date = fns.addDays(date, 1);
+  return getCompletionDate(date, businessDaysLater - 1, holidays);
+}
+
+/**
+ * 毎日が休日になっているか
+ */
+export function isAllDayOff(holidays: HolidaysOption) {
+  return Object.entries(holidays).every(([key, val]) =>
+    key === 'jp' ? true : val,
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,21 @@
 import { format } from 'date-fns';
 import ja from 'date-fns/locale/ja';
 import Head from 'next/head';
-import { ForwardedRef, forwardRef, useEffect, useState } from 'react';
+import {
+  ChangeEvent,
+  ForwardedRef,
+  forwardRef,
+  useCallback,
+  useEffect,
+  useState,
+} from 'react';
 import DatePicker from 'react-datepicker';
 import { AiFillCalendar } from 'react-icons/ai';
 import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
 import { ImArrowDown } from 'react-icons/im';
 
 import Layout from '../components/layouts/Layout';
-import { getCompletionDate } from '../lib/days';
+import { HolidaysOption, getCompletionDate, isAllDayOff } from '../lib/days';
 
 import type { NextPageWithLayout } from 'next';
 
@@ -21,7 +28,7 @@ const CustomInput = forwardRef((props, ref: ForwardedRef<HTMLInputElement>) => (
       {...props}
       ref={ref}
       type="text"
-      className="form-input cursor-pointer pl-10"
+      className="text-input cursor-pointer pl-10"
       placeholder="Select date"
       readOnly
     />
@@ -32,12 +39,33 @@ CustomInput.displayName = 'CustomInput';
 const Home: NextPageWithLayout = () => {
   const [eventDay, setEventDay] = useState(new Date(Date.now()));
   const [howManyDays, setHowManyDays] = useState('3');
+  const [holidays, setHolidays] = useState<HolidaysOption>({
+    mon: false,
+    tue: false,
+    wed: false,
+    thu: false,
+    fri: false,
+    sat: true,
+    sun: true,
+    jp: true,
+  });
   const [resultDay, setResultDay] = useState(eventDay);
 
   useEffect(() => {
-    const completionDate = getCompletionDate(eventDay, parseInt(howManyDays));
+    const completionDate = getCompletionDate(
+      eventDay,
+      parseInt(howManyDays),
+      holidays,
+    );
     setResultDay(new Date(completionDate));
-  }, [eventDay, howManyDays]);
+  }, [eventDay, howManyDays, holidays]);
+
+  const onChangeCheckbox = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setHolidays(prev => {
+      const next = { ...prev, [e.target.id]: e.target.checked };
+      return isAllDayOff(next) ? prev : next;
+    });
+  }, []);
 
   return (
     <>
@@ -53,7 +81,7 @@ const Home: NextPageWithLayout = () => {
           <h1 className="select-none pb-8 text-xl">○営業日後はいつ？</h1>
 
           <form className="space-y-4">
-            <label className="form-label">
+            <label>
               基準日
               <DatePicker
                 locale={ja}
@@ -70,7 +98,7 @@ const Home: NextPageWithLayout = () => {
               />
             </label>
 
-            <label className="form-label">
+            <label className="block">
               何営業日後？
               <div className="relative">
                 <input
@@ -83,13 +111,115 @@ const Home: NextPageWithLayout = () => {
                     const next = value ? (value < 10_000 ? value : 10_000) : 0;
                     setHowManyDays(next.toFixed(0));
                   }}
-                  className="form-input pl-4 pr-32"
+                  className="text-input pl-4 pr-32"
                 />
                 <div className="pointer-events-none absolute inset-y-0 right-4 flex items-center pl-3">
                   営業日後
                 </div>
               </div>
             </label>
+
+            <fieldset>
+              <legend>休日</legend>
+              <ul className="mt-1 flex w-full select-none flex-wrap gap-x-4 gap-y-2">
+                <li>
+                  <input
+                    id="mon"
+                    type="checkbox"
+                    className="peer hidden"
+                    checked={holidays.mon}
+                    onChange={onChangeCheckbox}
+                  />
+                  <label htmlFor="mon" className="checkbox-label">
+                    月
+                  </label>
+                </li>
+                <li>
+                  <input
+                    id="tue"
+                    type="checkbox"
+                    className="peer hidden"
+                    checked={holidays.tue}
+                    onChange={onChangeCheckbox}
+                  />
+                  <label htmlFor="tue" className="checkbox-label">
+                    火
+                  </label>
+                </li>
+                <li>
+                  <input
+                    id="wed"
+                    type="checkbox"
+                    className="peer hidden"
+                    checked={holidays.wed}
+                    onChange={onChangeCheckbox}
+                  />
+                  <label htmlFor="wed" className="checkbox-label">
+                    水
+                  </label>
+                </li>
+                <li>
+                  <input
+                    id="thu"
+                    type="checkbox"
+                    className="peer hidden"
+                    checked={holidays.thu}
+                    onChange={onChangeCheckbox}
+                  />
+                  <label htmlFor="thu" className="checkbox-label">
+                    木
+                  </label>
+                </li>
+                <li>
+                  <input
+                    id="fri"
+                    type="checkbox"
+                    className="peer hidden"
+                    checked={holidays.fri}
+                    onChange={onChangeCheckbox}
+                  />
+                  <label htmlFor="fri" className="checkbox-label">
+                    金
+                  </label>
+                </li>
+                <li>
+                  <input
+                    id="sat"
+                    type="checkbox"
+                    className="peer hidden"
+                    checked={holidays.sat}
+                    onChange={onChangeCheckbox}
+                  />
+                  <label htmlFor="sat" className="checkbox-label">
+                    土
+                  </label>
+                </li>
+                <li>
+                  <input
+                    id="sun"
+                    type="checkbox"
+                    className="peer hidden"
+                    checked={holidays.sun}
+                    onChange={onChangeCheckbox}
+                  />
+                  <label htmlFor="sun" className="checkbox-label">
+                    日
+                  </label>
+                </li>
+                <li>
+                  <input
+                    id="jp"
+                    type="checkbox"
+                    className="peer hidden"
+                    checked={holidays.jp}
+                    onChange={onChangeCheckbox}
+                  />
+                  <label htmlFor="jp" className="checkbox-label">
+                    日本の祝日
+                  </label>
+                </li>
+              </ul>
+            </fieldset>
           </form>
 
           <div className="my-6 flex w-full justify-center">
