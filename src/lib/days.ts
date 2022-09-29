1b3e98710af46eb724b0c87f1306d8d307ec37e7
derodero24 <k.itsh20524abc@gmail.com>
2022-09-29 11:33:31 +0900

    update

--- src/lib/days.ts
+++ src/lib/days.ts
@@ -1,4 +1,5 @@
-import { addBusinessDays, format, parseISO } from 'date-fns';
+import * as holiday_jp from '@holiday-jp/holiday_jp';
+import { addBusinessDays } from 'date-fns';
 
 /**
  * 作業完了予定日を返す
@@ -10,8 +11,7 @@ export const getCompletionDate = (
   businessDaysLater: number,
 ) => {
   // 基準日から◯営業日以降なので、本日+1日からスタート
-  const date = format(addBusinessDays(eventDay, 1), 'yyyy-MM-dd');
-  return shiftDate(date, businessDaysLater, 0);
+  return shiftDate(addBusinessDays(eventDay, 1), businessDaysLater, 0);
 };
 
 /**
@@ -22,12 +22,12 @@ export const getCompletionDate = (
  * @returns
  */
 const shiftDate = (
-  date: string,
+  date: Date,
   businessDaysLater: number,
   count: number,
-): string => {
-  if (JapaneseHolidays.includes(date)) {
-    date = getNextDate(date);
+): Date => {
+  if (holiday_jp.isHoliday(date)) {
+    date = addBusinessDays(date, 1);
     return shiftDate(date, businessDaysLater, count);
   }
 
@@ -36,64 +36,6 @@ const shiftDate = (
     return date;
   }
 
-  date = getNextDate(date);
-
+  date = addBusinessDays(date, 1);
   return shiftDate(date, businessDaysLater, count);
 };
-
-/**
- * 翌営業日を返す
- * @param date
- * @returns
- */
-const getNextDate = (date: string) => {
-  return format(
-    addBusinessDays(
-      new Date(
-        parseISO(date).getFullYear(),
-        parseISO(date).getMonth(),
-        parseISO(date).getDate(),
-      ),
-      1,
-    ),
-    'yyyy-MM-dd',
-  );
-};
-
-// 内閣府から日本の祝日をCSVでダウンロード出来るので、とりあえずその内容を使う
-// https://www8.cao.go.jp/chosei/shukujitsu/gaiyou.html
-const JapaneseHolidays: string[] = [
-  '2022-01-01',
-  '2022-01-10',
-  '2022-02-11',
-  '2022-02-23',
-  '2022-03-21',
-  '2022-04-29',
-  '2022-05-03',
-  '2022-05-04',
-  '2022-05-05',
-  '2022-07-18',
-  '2022-08-11',
-  '2022-09-19',
-  '2022-09-23',
-  '2022-10-10',
-  '2022-11-03',
-  '2022-11-23',
-  '2023-01-01',
-  '2023-01-02',
-  '2023-01-09',
-  '2023-02-11',
-  '2023-02-23',
-  '2023-03-21',
-  '2023-04-29',
-  '2023-05-03',
-  '2023-05-04',
-  '2023-05-05',
-  '2023-07-17',
-  '2023-08-11',
-  '2023-09-18',
-  '2023-09-23',
-  '2023-10-09',
-  '2023-11-03',
-  '2023-11-23',
-];
