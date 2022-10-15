ce420983cef0900b8448697221e6869155fa548e
derodero24 <k.itsh20524abc@gmail.com>
2022-10-15 14:23:53 +0900

    update icons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,34 @@ export default function MyDocument() {
   return (
     <Html>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <meta name="author" content="@derodero24" />
+
+        {/* Colors */}
+        <meta name="theme-color" content="#1f2937" />
+        <meta
+          name="theme-color"
+          media="(prefers-color-scheme: light)"
+          content="#f3f4f6"
+        />
+        <meta
+          name="theme-color"
+          media="(prefers-color-scheme: dark)"
+          content="#1f2937"
+        />
+
+        {/* icons */}
+        <link rel="icon" sizes="any" href="/favicon.ico" />
+        <link rel="icon" type="image/svg+xml" href="/icon.svg" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.json" />
+
+        {/* Google Font */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap"
           rel="stylesheet"
