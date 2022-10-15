148f5dc6c4325c31862e504e0fcd7502da7165a6
derodero24 <k.itsh20524abc@gmail.com>
2022-10-15 15:22:06 +0900

    support pwa

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+});
+
 const nextConfig = {
   reactStrictMode: true,
-  i18n: {
-    locales: ['ja', 'en'],
-    defaultLocale: 'ja',
-  },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
